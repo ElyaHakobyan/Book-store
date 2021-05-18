07c3cbe4ef74b57c5a430ea609545a934f1fa282
Elya <elya_hakobyan_2019@mail.ru>
2021-05-18 17:07:36 +0400

    initial commit

---
+++ Book.js
@@ -0,0 +1,57 @@
+
+
+// //var mydata = JSON.parse(data);
+
+//  //const data = require('./books.json')
+// //import data from '.\books.json'
+//  //console.log(Data);
+
+//  document.getElementById("get_the_file").addEventListener("change", function() {
+//     var file_to_read = document.getElementById("get_the_file").files[0];
+//     var fileread = new FileReader();
+//     fileread.onload = function(e) {
+//       var content = e.target.result;
+//       // console.log(content);
+//       var intern = JSON.parse(content); // Array of Objects.
+//       console.log(intern); // You can index every object
+//     };
+//     fileread.readAsText(file_to_read);
+//   });
+
+// fetch("./books.json")
+//   .then(function (response) {
+//     return response;
+//   })
+//   .then(function (data) {
+//     return data.json();
+//   })
+//   console.log(data.json);
+
+// fetch("./data.json")
+//   .then(function (response) {
+//     return response;
+//   })
+//   .then(function (data) {
+//     return data.json();
+//   })
+
+//   .then(function (Normal) {
+//     const html = Normal.map(
+//       (data) => `<div class="card"> 
+//         <h4> ${data.username}</h4>
+//         <h2> ${data.firstName}  ${data.lastName}</</h2>
+//         <h3> ${data.gender}</h3>
+//         <h3> ${data.aboutAuthor}</h3>
+//     </div>`
+//     );
+//     document.getElementById("app").innerHTML = html;
+//   })
+//   .catch(function (err) {
+//     console.log("Fetch problem show: " + err.message);
+//   });
+
+
+const 
+
+
+
